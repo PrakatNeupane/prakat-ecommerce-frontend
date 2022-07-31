a54f4a5259bf87b3e6f43e1b46936643c2d4ad02
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-07-31 18:15:08 +1000

    admin side menu done

--- src/components/admin-sidebar/AdminSidebar.jsx
+++ src/components/admin-sidebar/AdminSidebar.jsx
@@ -1,22 +1,74 @@
 import React, { useState } from "react";
+import { ListGroup } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { toggleSidebar } from "../../system-state/systemSlice";
 
 const AdminSidebar = () => {
-  const [show, setShow] = useState(true);
+  const dispatch = useDispatch();
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const { showAdminSidebar } = useSelector((state) => state.system);
 
   return (
     <>
-      <Offcanvas show={show} onHide={handleClose}>
+      <Offcanvas
+        show={showAdminSidebar}
+        onHide={() => dispatch(toggleSidebar())}
+      >
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          <Offcanvas.Title>Admin Side Menu</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+          <hr />
+          <ListGroup variant="flush" className="fs-5">
+            <ListGroup.Item>
+              <Link className="nav-link" to="/home">
+                <i class="fa-solid fa-house-chimney"></i> Home
+              </Link>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Link className="nav-link" to="/dashboard">
+                <i class="fa-solid fa-gauge"></i> Dashboard
+              </Link>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Link className="nav-link" to="/customers">
+                <i class="fa-solid fa-user"></i> Customers
+              </Link>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Link className="nav-link" to="/orders">
+                <i class="fa-solid fa-table-cells"></i> Orders
+              </Link>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Link className="nav-link" to="/products">
+                <i class="fa-brands fa-product-hunt"></i> Products
+              </Link>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Link className="nav-link" to="/categories">
+                <i class="fa-solid fa-sitemap"></i> Categories
+              </Link>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Link className="nav-link" to="/payments">
+                <i class="fa-solid fa-credit-card"></i> Payments
+              </Link>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Link className="nav-link" to="/setting">
+                <i class="fa-solid fa-gear"></i> Setting
+              </Link>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Link className="nav-link" to="/logout">
+                <i class="fa-solid fa-arrow-right-from-bracket"></i> Logout
+              </Link>
+            </ListGroup.Item>
+          </ListGroup>
         </Offcanvas.Body>
       </Offcanvas>
     </>
