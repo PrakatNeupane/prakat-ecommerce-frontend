9f3df767e8c985df2b33bfc76fc1bde11b37060c
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-08-15 16:42:40 +1000

    done refactoring

--- src/helpers/axiosHelpers.js
+++ src/helpers/axiosHelpers.js
@@ -2,14 +2,16 @@ import axios from "axios";
 
 const rootUrlAPI = "http://localhost:8000/api/v1";
 const adminEP = rootUrlAPI + "/admin";
+const catEP = rootUrlAPI + "/category"
 
-const apiProcessor = async ({ method, url, data }) => { // data must be object
+const apiProcessor = async ({ method, url, dataObj }) => { // data must be object
     try {
-        const response = await axios({
+        const { data } = await axios({
             method,
             url,
-            data,
+            data: dataObj,
         });
+        return data
     } catch (error) {
         let message = error.message;
         if (error.response && error.response.data) {
@@ -22,41 +24,25 @@ const apiProcessor = async ({ method, url, data }) => { // data must be object
     }
 }
 
-export const postUser = async (usrObj) => {
-    try {
-        const { data } = await axios.post(adminEP, usrObj);
-        return data;
-    } catch (error) {
-        console.log(error);
-        return {
-            status: "error",
-            message: error.message,
-        };
-    }
+export const postUser = async (dataObj) => {
+    const url = adminEP
+    return apiProcessor({ method: "post", url, dataObj })
 };
 
-export const postEmailVerification = async obj => {
-    try {
-        const { data } = await axios.post(adminEP + "/email-verification", obj)
-        return data
-    } catch (error) {
-        console.log(error)
-        return {
-            status: "error",
-            message: error.message,
-        }
-    }
+export const postEmailVerification = dataObj => {
+    const url = adminEP + "/email-verification"
+    return apiProcessor({ method: "post", url, dataObj })
 }
 
-export const loginUser = async (usrObj) => {
-    try {
-        const { data } = await axios.post(adminEP + "/login", usrObj);
-        return data;
-    } catch (error) {
-        console.log(error.response.data.message, error.message);
-        return {
-            status: "error",
-            message: error?.response?.data?.message || error.message,
-        };
-    }
+export const loginUser = (dataObj) => {
+    const url = adminEP + "/login"
+    return apiProcessor({ method: "post", url, dataObj })
 };
+
+
+// CATEGORY API
+
+export const getCategories = () => {
+    const url = catEP
+    return apiProcessor({ method: "get", url })
+}
--- src/pages/categories/categoryAction.js
+++ src/pages/categories/categoryAction.js
@@ -2,4 +2,5 @@ import { setCategories } from "./categorySlice";
 
 const fetchCategoriesAction = () => dispatch => {
     // call axios for api call
+
 }
