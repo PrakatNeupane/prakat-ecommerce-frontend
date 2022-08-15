3342ba317ed5dc5f0f216cbb1d82381a1a1b7642
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-08-15 16:46:17 +1000

    calling axios for api call

--- src/pages/categories/categoryAction.js
+++ src/pages/categories/categoryAction.js
@@ -1,6 +1,9 @@
+import { getCategories } from "../../helpers/axiosHelpers";
 import { setCategories } from "./categorySlice";
 
-const fetchCategoriesAction = () => dispatch => {
+export const fetchCategoriesAction = () => dispatch => {
     // call axios for api call
+    const response = await getCategories()
 
+    response.status === "success" && setCategories(response.result)
 }
